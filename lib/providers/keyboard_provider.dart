86d14a8726ff511bc7c7c65b08436bba74d3b72f
Kings Samuel <kingssamuel1997@gmail.com>
2023-05-11 00:06:09 +0100

    keyboard

--- lib/providers/keyboard_provider.dart
+++ lib/providers/keyboard_provider.dart
@@ -1,5 +1,85 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 class KeyboardProvider extends ChangeNotifier {
-  
+  String _input = '';
+  String _typed = '';
+  bool _isBackSpaceClicked = false;
+  bool _isHintClicked = false;
+  List<String> _playerNames = [];
+  List<String> _suggestions = [];
+
+  String get input => _input;
+  String get typed => _typed;
+  bool get isBackSpaceClicked => _isBackSpaceClicked;
+  bool get isHintClicked => _isHintClicked;
+  List<String> get suggestions => _suggestions;
+
+  void addInput(String input) {
+    _input += input;
+    notifyListeners();
+  }
+
+  void setTyped(String letter) async {
+    _typed = letter;
+    notifyListeners();
+    await Future.delayed(const Duration(milliseconds: 150));
+    _typed = '';
+    setSuggestions(_input);
+    notifyListeners();
+  }
+
+  void backSpace() {
+    if (_input.isNotEmpty) {
+      _input = _input.substring(0, _input.length - 1);
+      _isBackSpaceClicked = true;
+      notifyListeners();
+      Future.delayed(const Duration(milliseconds: 150), () {
+        _isBackSpaceClicked = false;
+        setSuggestions(_input);
+        notifyListeners();
+      });
+    }
+  }
+
+  void useHint() {
+    // TODO: implement useHint
+    _isHintClicked = true;
+    notifyListeners();
+    Future.delayed(const Duration(milliseconds: 150), () {
+      _isHintClicked = false;
+      notifyListeners();
+    });
+  }
+
+  void clearInput() {
+    _input = '';
+    _suggestions = [];
+    notifyListeners();
+  }
+
+  void setSuggestions(String input) {
+    _suggestions = _playerNames.where((element) => element.toLowerCase().contains(input.toLowerCase())).toList();
+  }
+
+  void useSuggestion(String suggestion) {
+    _input = suggestion.toUpperCase();
+    _suggestions = [];
+    notifyListeners();
+    Future.delayed(const Duration(minutes: 1), () {
+      _input = '';
+      notifyListeners();
+    });
+  }
+
+  // get all player names from player_names.json in assets
+  void getPlayerNames(BuildContext context) async {
+    final assetBundle = DefaultAssetBundle.of(context);
+    final data = await assetBundle.loadString('players_names.json');
+    final body = json.decode(data);
+
+    _playerNames = body.map<String>((json) => json['fullName'] as String).toList();
+
+    notifyListeners();
+  }
 }
--- lib/screens/game_play_screen.dart
+++ lib/screens/game_play_screen.dart
@@ -21,69 +21,212 @@ class GamePlayScreenState extends State<GamePlayScreen> {
   void initState() {
     super.initState();
     _keyboardProvider = context.read<KeyboardProvider>();
+    _keyboardProvider.getPlayerNames(context);
   }
 
   @override
   Widget build(BuildContext context) {
+    bool isDesktop = MediaQuery.of(context).size.width > 600;
+    bool isBackspaceClicked =
+        context.select((KeyboardProvider keyboardProvider) => keyboardProvider.isBackSpaceClicked);
+    bool isHintClicked = context.select((KeyboardProvider keyboardProvider) => keyboardProvider.isHintClicked);
+    String typedSelector = context.select((KeyboardProvider keyboardProvider) => keyboardProvider.typed);
+    String input = context.select((KeyboardProvider keyboardProvider) => keyboardProvider.input);
+    List<String> suggestions = context.select((KeyboardProvider keyboardProvider) => keyboardProvider.suggestions);
+
     return Scaffold(
-      backgroundColor: Palette.scaffold,
-      // keyboard
-      bottomNavigationBar: Container(
-        height: 200,
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        color: Palette.cardHeaderGrey,
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            // line 1
-            Row(
+        backgroundColor: Palette.scaffold,
+        body: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          // keyboard
+          Container(
+            height: 295,
+            width: isDesktop ? 500 : double.infinity,
+            padding: const EdgeInsets.only(top: 10, bottom: 20),
+            color: Palette.cardHeaderGrey,
+            alignment: Alignment.center,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                line1Letters.length,
-                (index) => Expanded(
-                  child: Container(
-                    margin: EdgeInsets.only(left: index == 0 ? 0 : 2, right: index == line1Letters.length - 1 ? 0 : 2),
-                    child: _key(line1Letters[index]),
+              children: [
+                // ** INPUT ** //
+                if (input.isEmpty)
+                  Center(child: bodyText(text: "Type in a PLAYER NAME", color: Colors.grey, fontSize: 18))
+                else
+                  Center(child: bodyText(text: input, color: Colors.white, fontSize: 18)),
+                const SizedBox(height: 15),
+                // ** SUGGESTIONS ** //
+                if (input.isNotEmpty)
+                  SizedBox(
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: suggestions.length,
+                      itemBuilder: (context, index) {
+                        String suggestion = suggestions[index];
+                        return GestureDetector(
+                          onTap: () => _keyboardProvider.useSuggestion(suggestion),
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 5),
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Palette.cardHeaderGrey,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: bodyText(
+                                text: suggestion.toUpperCase(),
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18,
+                                bold: true),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                // ** KEYBOARD ** //
+                // line 1
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(
+                    line1Letters.length,
+                    (index) => Expanded(
+                      child: Container(
+                        margin:
+                            EdgeInsets.only(left: index == 0 ? 0 : 2, right: index == line1Letters.length - 1 ? 0 : 2),
+                        child: _key(line1Letters[index]),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                const SizedBox(height: 5),
+                // line 2
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(
+                    line2Letters.length,
+                    (index) => Expanded(
+                        child: Container(
+                            margin: EdgeInsets.only(
+                                left: index == 0 ? 0 : 2, right: index == line1Letters.length - 1 ? 0 : 2),
+                            child: _key(line2Letters[index]))),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                // line 3
+                Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                  _key("-", width: 25),
+                  const SizedBox(width: 2),
+                  _key("'", width: 25),
+                  const SizedBox(width: 2),
+                  ...List.generate(
+                    line3Letters.length,
+                    (index) => Expanded(
+                        child: Container(
+                            margin: EdgeInsets.only(
+                                left: index == 0 ? 0 : 2, right: index == line1Letters.length - 1 ? 0 : 2),
+                            child: _key(line3Letters[index]))),
+                  ),
+                  const SizedBox(width: 2),
+                  // backspace
+                  InkWell(
+                    onTap: () => _keyboardProvider.backSpace(),
+                    onLongPress: () => _keyboardProvider.clearInput(),
+                    child: Container(
+                      width: 55,
+                      height: 40,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: isBackspaceClicked ? Palette.primary : Palette.scaffold,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Center(child: Icon(Icons.backspace, color: Colors.white)),
+                    ),
+                  ),
+                ]),
+                // line 4
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    // hint button
+                    InkWell(
+                      onTap: () => _keyboardProvider.useHint(),
+                      child: Container(
+                          height: 40,
+                          margin: const EdgeInsets.only(left: 3, right: 3),
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: isHintClicked ? Palette.primary : Palette.cardBodyGrey,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(Icons.lightbulb, color: Palette.brightYellow),
+                              const SizedBox(width: 5),
+                              bodyText(text: 'Hint', color: Colors.white, fontSize: 18),
+                            ],
+                          )),
+                    ),
+                    // spacebar
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          _keyboardProvider.setTyped(' ');
+                          _keyboardProvider.addInput(' ');
+                        },
+                        child: Container(
+                          height: 40,
+                          margin: const EdgeInsets.only(left: 3, right: 3),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: typedSelector == " " ? Palette.primary : Palette.cardBodyGrey,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: bodyText(text: 'Space', color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    // enter
+                    Visibility(
+                      visible: false,
+                      child: InkWell(
+                        onTap: () {}, //  TODO: add enter function
+                        child: Container(
+                          height: 40,
+                          margin: const EdgeInsets.only(left: 3, right: 3),
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Palette.primary,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: bodyText(text: 'Guess', color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-            const SizedBox(height: 10),
-            // line 2
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                line2Letters.length,
-                (index) => _key(line2Letters[index]),
-              ),
-            ),
-            const SizedBox(height: 10),
-            // line 3
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                line3Letters.length,
-                (index) => _key(line3Letters[index]),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ]));
   }
 
-  Widget _key(String letter) {
+  Widget _key(String letter, {double width = 40}) {
+    String typedSelector = context.select((KeyboardProvider keyboardProvider) => keyboardProvider.typed);
+
     return InkWell(
       onTap: () {
-        print(letter);
+        _keyboardProvider.setTyped(letter);
+        _keyboardProvider.addInput(letter);
       },
-      splashColor: Palette.primary,
       child: Container(
-          width: 40,
+          width: width,
           height: 40,
           alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: Palette.scaffold,
+            color: typedSelector == letter ? Palette.primary : Palette.scaffold,
             borderRadius: BorderRadius.circular(5),
           ),
           child: Center(child: bodyText(text: letter, color: Colors.white, fontSize: 18))),
